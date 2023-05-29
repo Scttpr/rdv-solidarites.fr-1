7aba1b8ce3125a16fbbd2500bf1afbff55bf6310
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2023-05-30 00:00:37 +0200

    Fix another spec

--- spec/controllers/admin/territories/agent_roles_controller_spec.rb
+++ spec/controllers/admin/territories/agent_roles_controller_spec.rb
@@ -40,19 +40,26 @@ describe Admin::Territories::AgentRolesController, type: :controller do
     end
   end
 
-  describe "#destoy" do
-    it "redirect to territorial_agent_edit on success if agent has another organisation" do
-      territory = create(:territory)
-      organisation = create(:organisation, territory: territory)
-      organisation2 = create(:organisation, territory: territory)
-      agent = create(:agent, role_in_territories: [territory])
-      create(:agent_territorial_access_right, territory: territory, agent: agent)
-      agent_role = create(:agent_role, organisation: organisation, agent: agent, level: "basic")
-      create(:agent_role, organisation: organisation2, agent: agent, level: "basic")
+  describe "DELETE #destroy" do
+    let(:territory) { create(:territory) }
+    let(:organisation) { create(:organisation) }
 
+    before do
+      # Il doit toujours y avoir au moins un agent responsable par territoire
+      create(:agent, territories: [territory])
+
+      # Il doit toujours y avoir au moins un agent Admin par organisation
       last_agent = create(:agent)
       create(:agent_territorial_access_right, territory: territory, agent: last_agent)
       create(:agent_role, organisation: organisation, agent: last_agent, level: "admin")
+    end
+
+    it "redirect to territorial_agent_edit on success if agent has another organisation" do
+      agent = create(:agent, role_in_territories: [territory])
+      create(:agent_territorial_access_right, territory: territory, agent: agent)
+      agent_role = create(:agent_role, organisation: organisation, agent: agent, level: "basic")
+      organisation2 = create(:organisation, territory: territory)
+      create(:agent_role, organisation: organisation2, agent: agent, level: "basic")
 
       sign_in agent
 
@@ -61,15 +68,9 @@ describe Admin::Territories::AgentRolesController, type: :controller do
     end
 
     it "redirect to territorial_agent_edit on success if it the last organisation" do
-      territory = create(:territory)
-      organisation = create(:organisation, territory: territory)
       agent = create(:agent, role_in_territories: [territory])
       create(:agent_territorial_access_right, territory: territory, agent: agent)
       agent_role = create(:agent_role, organisation: organisation, agent: agent, level: "basic")
-
-      last_agent = create(:agent)
-      create(:agent_territorial_access_right, territory: territory, agent: last_agent)
-      create(:agent_role, organisation: organisation, agent: last_agent, level: "admin")
 
       sign_in agent
 
@@ -78,16 +79,9 @@ describe Admin::Territories::AgentRolesController, type: :controller do
     end
 
     it "destroy agent_role" do
-      territory = create(:territory)
-      organisation = create(:organisation, territory: territory)
       agent = create(:agent, role_in_territories: [territory])
       create(:agent_territorial_access_right, territory: territory, agent: agent)
       agent_role = create(:agent_role, organisation: organisation, agent: agent, level: "basic")
-
-      # Il doit toujours y avoir un dernier admin dans une organisation pour le moment
-      last_agent = create(:agent)
-      create(:agent_territorial_access_right, territory: territory, agent: last_agent)
-      create(:agent_role, organisation: organisation, agent: last_agent, level: "admin")
 
       sign_in agent
 
